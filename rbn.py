a87b409cdfb02a704def39636ada9a71891bec2a
yaoxr <suanrong@gmail.com>
2017-01-15 19:47:02 +0800

    accomplish rbm

--- rbn.py
+++ rbn.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from util import getData
 
 class rbm:
     def __init__(self, shape, para, data):
@@ -17,20 +18,30 @@ class rbm:
         print "rbm init completely"
         pass
     def buildModel(self):
-        self.hidden_holder = tf.placeholder("float", [None, shape[1]])
-        self.visual_holder = tf.placeholder("float", [None, shape[0]])
-        self.hidden = tf.nn.sigmoid(self.visual_holder * self.W + self.bh)
-        self.visual = tf.nn.sigmoid(self.hidden_holder * tf.transpose(self.W) + self.bv)
-        self.random = tf.Variable()
+        self.v = tf.placeholder("float", [None, shape[0]])
+        self.h = self.sample(tf.sigmoid(self.v * self.W + self.bh))
+        #gibbs_sample
+        v_sample = self.sample(tf.sigmoid(self.h * tf.transpose(self.W) + self.bv))
+        h_sample = self.sample(tf.sigmoid(self.v_sample * self.W + self.bh))
+        self.lr = self.para["learning_rate"] / tf.to_float(self.para["batch_size"])
+        W_adder = self.W.assign_add(self.lr  * (tf.transpose(v) * h - tf.transpose(v_sample) * h_sample))
+        bv_adder = self.bv.assign_add(self.lr * tf.reduce_mean(v - v_sample, 0))
+        bh_adder = self.bh.assign_add(self.lr * tf.reduce_mean(h - h_sample, 0))
+        self.upt = [W_adder, bv_adder, bh_adder]
     def doTrain(self):
         for epoch in range(self.para["epoch"]):
             np.random.shuffle(self.data)
             for i in range(0, len(self.data), self.para["batch_size"]):
                 X = self.data[i:i + self.para["batch_size"]]
-                self.sess.run(upt, feed_dict = {self.visual_holder : X})
+                self.sess.run(self.upt, feed_dict = {self.v : X})
         pass
-    def sample(probs):
+    def sample(self, probs):
         return tf.floor(probs + tf.random_uniform(tf.shape(probs), 0, 1))
-    def gibbs_sample(k);
-        
-    def cdK(self)
+    def getWb(self):
+        return self.sess.run([self.W, self.bv, self.bh])
+
+if __name__ == "__main__":
+    data = getData(dataSet)
+    myRBM = rbm([data.shape[0], 200], {"batch_size": 64, "learning_rate":0.001}, data)
+    myRBM.doTrain()
+    W, bv, bh = myRBM.getWb()
