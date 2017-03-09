68e4502d6fa0ffe1dca8e5cfba8e7cfd28f434ac
yxr <suanrong@gmail.com>
2017-03-09 12:05:44 +0800

    EDIT: calc setting time in test_matmul.py

--- test_matmul.py
+++ test_matmul.py
@@ -1,4 +1,5 @@
-
+import tensorflow as tf
+import numpy as np
 import time
 import copy
 import random
@@ -9,7 +10,7 @@ def setPara():
     para = {}
     para["trainingEpochs"] = 30
     para["batchSize"] = 1000
-    para["sparse_dot"] = False
+    para["sparse_dot"] = True
     return para
 
 def comparison(data, para):
@@ -28,6 +29,7 @@ def comparison(data, para):
         all_time = 0 
         for i in range(total_batch):
             print "batch %d:" % i
+            stT = time.time()
             st = i * para["batchSize"]
             en =(i+1) * para["batchSize"]
             index = order[st:en]
@@ -39,17 +41,15 @@ def comparison(data, para):
                 x_val = batchX1[np.where(batchX1)]
                 tensor = tf.SparseTensor(x_ind, x_val, x_shape)
                 x = tf.nn.sigmoid(tf.sparse_tensor_dense_matmul(tensor, W) + b)
-                stT = time.time()
+                print "setting time: %.3fs" % (time.time() - stT)
                 _ = sess.run(x)
-                mini_time = time.time - stT
-                all_time = all_time + mini_time
             else:
                 tensor = tf.constant(batchX1)
                 x = tf.nn.sigmoid(tf.matmul(tensor, W) + b)
-                stT = time.time()
+                print "setting time: %.3fs" % (time.time() - stT)
                 _ = sess.run(x)
-                mini_time = time.time - stT
-                all_time = all_time + mini_time
+            mini_time = time.time() - stT
+            all_time = all_time + mini_time
             print "minibatch time: %.3fs" % mini_time 
         wholetime = wholetime + all_time
         print " wholebatch time : %.3fs" % wholetime
